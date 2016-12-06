92254b098eb6f6fd94f8435e0f579d8e69837534
mani2308 <mani.karl.gudmundsson@gmail.com>
2016-12-06 08:54:23 +0000

    Update tetris.py

--- lokaverk/tetris.py
+++ lokaverk/tetris.py
@@ -1,12 +1,9 @@
-# coding=utf-8
 from random import randrange as rand
 from Testerino import Connection
-# Tekur inn Connection fra Testerino.py til að fa connection vid database
 import pygame, sys
-# Importar inn pygame og sys semer notað aðeins neðar i koðanum
 
 
-con = Connection()              # Byr til breytu fyrir Connection()
+con = Connection()
 # Controles:
 #       Down - Kubbarnir fara hradar nidur
 # Left/Right - Faerir Kubbinn/ana til haegri/vinstri
